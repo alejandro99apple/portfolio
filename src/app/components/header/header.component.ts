43b835a976f1ef6d4e5f3d3c4212246d4e616321
alejandro99apple <alejandro99apple@gmail.com>
2024-12-31 00:26:23 -0600

    almost second video

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.addClickEventToLinks();
+  }
+
+  addClickEventToLinks(): void {
+    const links = document.querySelectorAll('.navbar-nav .nav-link');
+    const navbarCollapse = document.querySelector('.navbar-collapse') as HTMLElement;
+
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        if (navbarCollapse.classList.contains('show')) {
+          navbarCollapse.classList.remove('show');
+        }
+      });
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const navbarCollapse = document.querySelector('.navbar-collapse') as HTMLElement;
+    const target = event.target as HTMLElement;
+
+    if (!navbarCollapse.contains(target) && navbarCollapse.classList.contains('show')) {
+      navbarCollapse.classList.remove('show');
+    }
+  }
 }
