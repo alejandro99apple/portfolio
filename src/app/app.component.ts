dafdc76a00f2c9e36b18057309f55d27f48b9feb
alejandro99apple <alejandro99apple@gmail.com>
2024-12-30 23:05:02 -0600

    almost second video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgScrollbarModule } from 'ngx-scrollbar';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from "./components/home/home.component";
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactMeComponent } from "./components/contact-me/contact-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    NgScrollbarModule,
     HeaderComponent,
     HomeComponent,
     ProjectsComponent,
     FooterComponent,
+    ContactMeComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
